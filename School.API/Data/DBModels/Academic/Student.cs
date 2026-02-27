62265562d09010f92f308f9bf22ca7a61ef90364
naveddbajwa62 <naveedbajwaa62@gmail.com>
2026-02-28 00:45:33 +0500

    Student Moell Class

--- School.API/Data/DBModels/Academic/Student.cs
+++ School.API/Data/DBModels/Academic/Student.cs
@@ -6,15 +6,23 @@ namespace School.API.Data.DBModels.Accounts
     {
         [Key]
         public int VID { get; set; }
+        public int ParentID { get; set; }
+
         public string RegistrationNo { get; set; }
 
         public string AdmissionNo { get; set; }
 
         public DateTime? AdmissionDate { get; set; }
 
+        public string RollNo { get; set; }
+
         public string Name { get; set; }
 
         public string Name_Urdu { get; set; }
+
+        public string MobileNo { get; set; }
+
+        public bool? IsSMS { get; set; }
 
         public string Gender { get; set; }
 
@@ -30,43 +38,60 @@ namespace School.API.Data.DBModels.Accounts
 
         public string Address { get; set; }
 
-        public bool? StudentActive { get; set; }
-
-        public int ParentID { get; set; }
-
         public string FatherName { get; set; }
+
+        public string FatherNameUrdu { get; set; }
 
         public string FatherCNIC { get; set; }
 
         public string FatherMobile { get; set; }
 
+        public bool? IsFatherSMS { get; set; }
+
         public string Occupation { get; set; }
 
         public string MotherName { get; set; }
 
+        public string MotherNameUrdu { get; set; }
+
+        public string MotherCNIC { get; set; }
+
         public string MotherMobile { get; set; }
 
+        public bool? IsMotherSMS { get; set; }
+
         public string GuardianName { get; set; }
+
+        public string GuardianNameUrdu { get; set; }
 
         public string GuardianCNIC { get; set; }
 
         public string GuardianMobile { get; set; }
 
+        public bool? IsGuardianSMS { get; set; }
+
         public string GuardianRelation { get; set; }
-
-        public bool? ParentActive { get; set; }
-
-        public int AcademicID { get; set; }
 
         public int SessionID { get; set; }
 
         public int SectionID { get; set; }
 
-        public string RollNo { get; set; }
+        public string ClassRollNo { get; set; }
 
         public int? GradeID { get; set; }
 
-        public bool? AcademicActive { get; set; }
+        public int? StatusID { get; set; }
+
+        public bool? IsAvailAcademy { get; set; }
+
+        public string SectionName { get; set; }
+
+        public int ClassID { get; set; }
+
+        public string ClassName { get; set; }
+
+        public string SessionName { get; set; }
+
         public bool IsActive { get; set; } = true;
 
         public int? InsertedBy { get; set; }
