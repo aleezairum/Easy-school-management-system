8ef3b542ed43ca5472d7ad1f1c06b8812372cfcc
naveddbajwa62 <naveedbajwaa62@gmail.com>
2026-03-03 00:31:25 +0500

    Apply Fee

--- School.Web/wwwroot/js/common.js
+++ School.Web/wwwroot/js/common.js
@@ -1,5 +1,5 @@
 ﻿// For Live
-const API_BASE = 'http://68.178.166.149:5266/api';
+//const API_BASE = 'http://68.178.166.149:5266/api';
 
 // For Local
-//const API_BASE = 'http://localhost:5266/api';
+const API_BASE = 'http://localhost:5266/api';
